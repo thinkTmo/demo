645a3c88427a6fa88dc218cf326d5686ad58b5be
thinkSomething <1713783817@qq.com>
2015-06-20 20:40:15 +0800

    3

--- js/index.js
+++ js/index.js
@@ -1,10 +1,11 @@
 var Index = {
     setTarget: function () {
         var a = document.getElementsByTagName("a"),
-            frame = document.getElementsByTagName("iframe")[0];
+            frame = document.getElementsByTagName("iframe")[0],
+            fName = frame.getAttribute("name");
         for (var i = 0; i < a.length; i++) {
             var ele = a[i];
-            a[i].setAttribute("target", frame.getAttribute("name"));
+            a[i].setAttribute("target", fName);
             a[i].setAttribute("title", a[i].innerHTML);
             a[i].addEventListener("click", function () {
                 Index.bindClickToA(ele);
@@ -12,8 +13,12 @@ var Index = {
         }
     },
     bindClickToA: function (obj) {
-        
-        $(obj).parent(".js_first_ul").find("a")
-        
+        var title = document.getElementsByTagName("title")[0];
+        title.innerHTML = obj.innerHTML;
+        $(obj).parentsUntil(".js_first_ul").find("a").removeClass("selected");
+
+        //        $(obj).addClass("selected");
+
+
     }
 }
